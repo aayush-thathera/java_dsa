769cec433abe6fba28f9f2f965aba9995f3412d0
aayush-thathera <aayushthathera6gmail.com>
2025-04-25 13:50:39 +0530

    add

---
+++ NumberGuessingGamee.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int secretNumber = random.nextInt(100) + 1; // Random number between 1 and 100
+        int guess;
+        int attempts = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+        System.out.println("Guess a number between 1 and 100");
+
+        do {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
+
+            if (guess < secretNumber) {
+                System.out.println("Too low! Try again.");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high! Try again.");
+            } else {
+                System.out.println("🎉 Correct! You guessed it in " + attempts + " attempts.");
+            }
+        } while (guess != secretNumber);
+
+        scanner.close();
+    }
+}
