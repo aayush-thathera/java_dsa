8c87c476d60157f846ad22b7daf837bea5e30799
aayush-thathera <aayushthathera6gmail.com>
2025-05-10 23:12:48 +0530

    ArmstrongNumbe

---
+++ ArmstrongNumber.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        int originalNum = num;
+        int result = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            result += digit * digit * digit; // cube of the digit
+            num /= 10;
+        }
+
+        if (result == originalNum) {
+            System.out.println(originalNum + " is an Armstrong number.");
+        } else {
+            System.out.println(originalNum + " is not an Armstrong number.");
+        }
+
+        scanner.close();
+    }
+}
