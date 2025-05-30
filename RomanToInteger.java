c3cd3d850fd5f90ed71f2d8ab57c6c744cbfca2e
aayush-thathera <aayushthathera6gmail.com>
2025-05-30 09:22:18 +0530

    RomanToInteger

---
+++ RomanToInteger.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int total = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            int currentValue = romanMap.get(s.charAt(i));
+            if (currentValue < prevValue) {
+                total -= currentValue;
+            } else {
+                total += currentValue;
+            }
+            prevValue = currentValue;
+        }
+
+        return total;
+    }
+
+    public static void main(String[] args) {
+        // Test input
+        String roman = "III";
+        int result = romanToInt(roman);
+        System.out.println("Roman numeral " + roman + " = " + result);
+    }
+}
